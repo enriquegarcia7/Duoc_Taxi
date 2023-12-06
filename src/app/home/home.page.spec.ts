944533b4f306490510a866b9c6958ea6a47bfaf5
Enrique García <107608830+enriquegarcia7@users.noreply.github.com>
2023-12-06 00:32:23 -0300

    t

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,24 +1,89 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
 import { HomePage } from './home.page';
+import { AlertController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from "@angular/router";
+import { AppComponent } from '../app.component';
+import { GmapsService } from '../services/googmaps/gmaps.service';
+import { FbCarreraService } from "../services/FbCarrera/fb-carrera.service";
+
+class AlertControllerStub {
+  create(obj: any) {}
+}
+
+class AngularFireAuthStub {
+  currentUser = Promise.resolve({ emailVerified: true });
+}
+
+class RouterStub {
+  navigate(arr: string[]) {}
+}
+
+class AppComponentStub {
+  getGeolocation() {}
+}
+
+class GmapsServiceStub {
+  loadGoogleMaps() {}
+}
+
+class FbCarreraServiceStub {
+  addCarrera(obj: any) {}
+}
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let appComponent: AppComponent;
+  let gmapsService: GmapsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+      declarations: [ HomePage ],
+      providers: [
+        { provide: AlertController, useClass: AlertControllerStub },
+        { provide: AngularFireAuth, useClass: AngularFireAuthStub },
+        { provide: Router, useClass: RouterStub },
+        { provide: AppComponent, useClass: AppComponentStub },
+        { provide: GmapsService, useClass: GmapsServiceStub },
+        { provide: FbCarreraService, useClass: FbCarreraServiceStub },
+      ]
+    })
+    .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    appComponent = TestBed.inject(AppComponent);
+    gmapsService = TestBed.inject(GmapsService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call getGeolocation on ngOnInit', () => {
+    spyOn(appComponent, 'getGeolocation');
+    component.ngOnInit();
+    expect(appComponent.getGeolocation).toHaveBeenCalled();
+  });
+
+  it('should navigate to /registro if user is not verified', () => {
+    const afAuth = TestBed.inject(AngularFireAuth);
+    afAuth.currentUser = Promise.resolve({ emailVerified: false } as any);
+
+    const router = TestBed.inject(Router);
+    const spy = spyOn(router, 'navigate');
+
+    component.ngOnInit();
+
+    expect(spy).toHaveBeenCalledWith(['/registro']);
+  });
+
+  it('should call addCarrera when aplicar is called', () => {
+    const carreraService = TestBed.inject(FbCarreraService);
+    const spy = spyOn(carreraService, 'addCarrera').and.callThrough();
+
+    component.aplicar();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
--- src/app/login/login/login.page.spec.ts
+++ src/app/login/login/login.page.spec.ts
@@ -1,7 +1,46 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { LoginPage } from './login.page';
+import { ToastController } from '@ionic/angular';
+import { FirebaseErrorService } from "../../services/firebase-error/firebase-error.service";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { HttpClient } from "@angular/common/http";
+import { UsuarioService } from "../../services/usuario/usuario.service";
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from "@angular/router";
+import { AlertController } from '@ionic/angular';
+
+class ToastControllerStub {
+  create(obj: any) {}
+}
+
+class FirebaseErrorServiceStub {
+  codeError(code: string) {}
+}
+
+class AngularFireAuthStub {
+  signInWithEmailAndPassword(email: string, password: string) {
+    return Promise.resolve({ user: { emailVerified: true, uid: '123' } });
+  }
+}
+
+class HttpClientStub {}
+
+class UsuarioServiceStub {}
+
+class FormBuilderStub {
+  group(obj: any) {
+    return new FormGroup({});
+  }
+}
+
+class RouterStub {
+  navigate(arr: string[]) {}
+}
+
+class AlertControllerStub {
+  create(obj: any) {}
+}
 
 describe('LoginPage', () => {
   let component: LoginPage;
@@ -10,6 +49,16 @@ describe('LoginPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginPage ],
+      providers: [
+        { provide: ToastController, useClass: ToastControllerStub },
+        { provide: FirebaseErrorService, useClass: FirebaseErrorServiceStub },
+        { provide: AngularFireAuth, useClass: AngularFireAuthStub },
+        { provide: HttpClient, useClass: HttpClientStub },
+        { provide: UsuarioService, useClass: UsuarioServiceStub },
+        { provide: FormBuilder, useClass: FormBuilderStub },
+        { provide: Router, useClass: RouterStub },
+        { provide: AlertController, useClass: AlertControllerStub },
+      ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
@@ -20,5 +69,14 @@ describe('LoginPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call signInWithEmailAndPassword when log is called', () => {
+    const afAuth = TestBed.inject(AngularFireAuth);
+    const spy = spyOn(afAuth, 'signInWithEmailAndPassword').and.callThrough();
+
+    component.log();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
